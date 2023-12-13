183f580addb7f99db18c7897d3dee1c9991f6363
clay <claytonharting+github@gmail.com>
2023-12-13 03:26:17 -0500

    added lots more dialogue

--- DialogueManifest.cs
+++ DialogueManifest.cs
@@ -25,34 +25,12 @@ namespace PhilipTheMechanic
         public void LoadManifest(IStoryRegistry storyRegistry)
         {
             LoadShouts(storyRegistry);
+            LoadOutsideOfCombatConversations(storyRegistry);
+            LoadShopConversations(storyRegistry);
         }
 
-        private void LoadShouts(IStoryRegistry storyRegistry)
+        private void LoadOutsideOfCombatConversations(IStoryRegistry storyRegistry)
         {
-            Logger.LogInformation("\n\n\n\nLOADING SHOUTS!\n\n\n\n");
-            RegisterSimpleShout(storyRegistry, "New loop, new ship to take apart!", "NewLoopShout", loopTag: "maniacal", zones: new HashSet<string>() { "zone_first" });
-            RegisterSimpleShout(storyRegistry, "Man the engines are running horribly, forget the fight, I'm gonna tune them.", "Engines", loopTag: "gone");
-            RegisterSimpleShout(storyRegistry, "Hey, who installed this cockpit without combat-rated safety glass? They should have their license revoked.", "SafetyGlass", loopTag: "unhappy", storyNode: StandardShoutHooks.Relevance3.ArtifactHardmode);
-            RegisterSimpleShout(storyRegistry, "Tsk, you really should've armored that weak point.", "EnemyWeakPoint", storyNode: StandardShoutHooks.Relevance7.EnemyHasWeakness);
-            RegisterSimpleShout(storyRegistry, "Woah your ship, uh... That brittle part needs to be replaced.", "EnemyBrittlePart", storyNode: StandardShoutHooks.Relevance7.EnemyHasBrittle);
-            RegisterSimpleShout(storyRegistry, "Yeeeeeaaah we don't have enough spare parts to fix that.", "DamageTaken", storyNode: StandardShoutHooks.Relevance7.ThatsALotOfDamageToUs);
-            RegisterSimpleShout(storyRegistry, "I hear whirring. Why do I hear whirring?", "HullLow", loopTag: "gameover", storyNode: StandardShoutHooks.Relevance8.Duo_AboutToDieAndLoop);
-            RegisterSimpleShout(storyRegistry, "Don't worry! I've got a tiny fire extinguisher!", "Heat", storyNode: new StoryNode()
-            {
-                goingToOverheat = true
-            });
-            RegisterSimpleShout(storyRegistry, "I knew that fire extinguisher would help.", "OverheatAnyoneFix", loopTag: "classy", storyNode: new StoryNode()
-            {
-                wasGoingToOverheatButStopped = true
-            });
-            RegisterSimpleShout(storyRegistry, "Woah ho!!", "LotsOfDamage", storyNode: StandardShoutHooks.Relevance6.WeDidOverThreeDamage);
-            RegisterSimpleShout(storyRegistry, "Thank you shields!", "PlayerCompletelyBlocked", storyNode: StandardShoutHooks.Relevance6.WeGotShotButTookNoDamage);
-            RegisterSimpleShout(storyRegistry, "Man, these engines are so good.", "PlayerEscaped", storyNode: StandardShoutHooks.Relevance6.WeDontOverlapWithEnemyAtAll);
-
-            //
-            // OUTSIDE OF COMBAT CONVERSATIONS
-            //
-
             storyRegistry.RegisterStory(new ExternalStory(
                 $"{Name}.IsaacHardhat",
                 node: new StoryNode()
@@ -129,11 +107,10 @@ namespace PhilipTheMechanic
                     },
                 }
             ));
+        }
 
-            //
-            // SHOP CONVERSATIONS
-            //
-
+        private void LoadShopConversations(IStoryRegistry storyRegistry)
+        {
             storyRegistry.RegisterStory(new ExternalStory(
                 $"{Name}.ShopNanobots",
                 node: new StoryNode()
@@ -198,11 +175,142 @@ namespace PhilipTheMechanic
                     }
                 }
             ));
+        }
+
+        private void LoadShouts(IStoryRegistry storyRegistry)
+        {
+            RegisterSimpleShout(storyRegistry, "New loop, new ship to take apart!", "NewLoopShout", loopTag: "maniacal", zones: new HashSet<string>() { "zone_first" });
+            RegisterSimpleShout(storyRegistry, "Man the engines are running horribly, forget the fight, I'm gonna tune them.", "Engines", loopTag: "gone");
+            RegisterSimpleShout(storyRegistry, "Hey, who installed this cockpit without combat-rated safety glass? They should have their license revoked.", "SafetyGlass", loopTag: "squint", storyNode: StandardShoutHooks.Relevance3.ArtifactHardmode);
+            RegisterSimpleShout(storyRegistry, "Tsk, you really should've armored that weak point.", "EnemyWeakPoint", loopTag: "classy", storyNode: StandardShoutHooks.Relevance7.EnemyHasWeakness);
+            RegisterSimpleShout(storyRegistry, "Woah your ship, uh... That brittle part needs to be replaced.", "EnemyBrittlePart", storyNode: StandardShoutHooks.Relevance7.EnemyHasBrittle);
+            RegisterSimpleShout(storyRegistry, "Yeeeeeaaah we don't have enough spare parts to fix that.", "DamageTaken", storyNode: StandardShoutHooks.Relevance7.ThatsALotOfDamageToUs);
+            RegisterSimpleShout(storyRegistry, "I hear whirring. Why do I hear whirring?", "HullLow", loopTag: "gameover", storyNode: StandardShoutHooks.Relevance8.Duo_AboutToDieAndLoop);
+            RegisterSimpleShout(storyRegistry, "Don't worry! I've got a tiny fire extinguisher!", "Heat", storyNode: new StoryNode()
+            {
+                goingToOverheat = true
+            });
+            RegisterSimpleShout(storyRegistry, "I knew that fire extinguisher would help.", "OverheatAnyoneFix", loopTag: "classy", storyNode: new StoryNode()
+            {
+                wasGoingToOverheatButStopped = true
+            });
+            RegisterSimpleShout(storyRegistry, "Woah ho!!", "LotsOfDamage", storyNode: StandardShoutHooks.Relevance6.WeDidOverThreeDamage);
+            RegisterSimpleShout(storyRegistry, "Everything's coming together!", "WeDidOverFiveDamage", loopTag: "maniacal", storyNode: StandardShoutHooks.Relevance7.WeDidOverFiveDamage);
+            RegisterSimpleShout(storyRegistry, "Thank you shields!", "PlayerCompletelyBlocked", storyNode: StandardShoutHooks.Relevance6.WeGotShotButTookNoDamage);
+            RegisterSimpleShout(storyRegistry, "Man, these engines are so good.", "PlayerEscaped", storyNode: StandardShoutHooks.Relevance6.WeDontOverlapWithEnemyAtAll);
+            RegisterSimpleShout(storyRegistry, "Oooh... fixing that corrosion damage is not gonna be fun.", "ImCorroded", loopTag: "unhappy", storyNode: StandardShoutHooks.Relevance6.WeAreCorroded);
+            RegisterSimpleShout(storyRegistry, "I really gotta get better at organization.", "Trash", loopTag: "sheepish", storyNode: StandardShoutHooks.Relevance6.HandOnlyHasTrashCards);
+            RegisterSimpleShout(storyRegistry, "Out of tools...", "Energy", loopTag: "unhappy", storyNode: StandardShoutHooks.Relevance8.EmptyHandWithEnergy);
+            RegisterSimpleShout(storyRegistry, "Solid hit!", "JustHit", storyNode: StandardShoutHooks.Relevance8.JustHitGeneric);
+            RegisterSimpleShout(storyRegistry, "Woah... Max, who's your parts supplier?", "ArtifactTridimensionalCockpit", storyNode: StandardShoutHooks.Relevance7.ArtifactTridimensionalCockpit);
+            RegisterSimpleShout(storyRegistry, "And here I thought I'd never get to use that woodworking class!", "ArtifactTiderunner", loopTag: "excited", storyNode: StandardShoutHooks.Relevance7.ArtifactTiderunner);
+            //RegisterSimpleShout(storyRegistry, "Easy repair!", "JustHit", loopTag: "classy", storyNode: StandardShoutHooks.Relevance8.JustHitGeneric);
+
+            DBPatches.RegisterStoryNodeModification("ShopKeepBattleInsult", (node) =>
+            {
+                (node.lines[0] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                    loopTag = "whatisthat"
+                });
+                DBPatches.RegisterLocalization("en", "ShopKeepBattleInsult:Philip", "...");
+            });
+
+            DBPatches.RegisterStoryNodeModification("CrabFacts1_Multi_0", (node) =>
+            {
+                // lines[0] are the crab facts, lines[1] are the crew's responses
+                (node.lines[1] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip"
+                });
+                DBPatches.RegisterLocalization("en", "CrabFacts1_Multi_0:Philip", "Wow! Now I want to be a crabologist.");
+            });
+            DBPatches.RegisterStoryNodeModification("CrabFacts2_Multi_0", (node) =>
+            {
+                // lines[0] are the crab facts, lines[1] are the crew's responses
+                (node.lines[1] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                    loopTag = "squint"
+                });
+                DBPatches.RegisterLocalization("en", "CrabFacts2_Multi_0:Philip", "I don't get it.");
+            });
+            DBPatches.RegisterStoryNodeModification("CrabFactsAreOverNow_Multi_0", (node) =>
+            {
+                // lines[0] are the crab facts, lines[1] are the crew's responses
+                (node.lines[1] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                    loopTag = "classy"
+                });
+                DBPatches.RegisterLocalization("en", "CrabFactsAreOverNow_Multi_0:Philip", "Guess I'll have to find my own now.");
+            });
+            DBPatches.RegisterStoryNodeModification("Soggins_Missile_Shout_1", (node) =>
+            {
+                (node.lines[1] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                    loopTag = "laugh"
+                });
+                DBPatches.RegisterLocalization("en", "Soggins_Missile_Shout_1:Philip", "Reminds me of when I was an apprentice!");
+            });
+            DBPatches.RegisterStoryNodeModification("SogginsEscapeIntent_1", (node) =>
+            {
+                (node.lines[1] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                });
+                DBPatches.RegisterLocalization("en", "SogginsEscapeIntent_1:Philip", "Classic solution.");
+            });
+            DBPatches.RegisterStoryNodeModification("WeJustGainedHeatAndDrakeIsHere_Multi_0", (node) =>
+            {
+                (node.lines[0] as SaySwitch).lines.Add(new Say()
+                {
+                    who = Philip,
+                    hash = "Philip",
+                    loopTag = "unhappy"
+                });
+                DBPatches.RegisterLocalization("en", "WeJustGainedHeatAndDrakeIsHere_Multi_0:Philip", "You're not making my job easy here, Drake.");
+            });
+
 
             //
             // RANDOM MID-COMBAT CONVERSATIONS
             //
 
+            storyRegistry.RegisterStory(new ExternalStory(
+                $"{Name}.KickTheirButt",
+                node: new StoryNode()
+                {
+                    type = NodeType.@combat,
+                    priority = false,
+                    playerShotJustHit = true,
+                    minDamageDealtToEnemyThisAction = 1,
+                    allPresent = new() { Philip, "shard" },
+                },
+                instructions: new List<object>()
+                {
+                    new ExternalStory.ExternalSay()
+                    {
+                        Who = "shard",
+                        What = "KICK THEIR BUTT!",
+                        LoopTag = "squint",
+                    },
+                    new ExternalStory.ExternalSay()
+                    {
+                        Who = Philip,
+                        What = "Yeah, you tell 'em Books!!",
+                        LoopTag = "excited"
+                    },
+                }
+            ));
+            
             storyRegistry.RegisterStory(new ExternalStory(
                 $"{Name}.SelfPropellingCannons",
                 node: new StoryNode()
@@ -257,40 +365,34 @@ namespace PhilipTheMechanic
                         Who = "peri",
                         What = "Yes.",
                         LoopTag = "squint",
-                        Delay = 1
                     },
                     new ExternalStory.ExternalSay()
                     {
                         Who = "shard",
                         What = "I don't think so!",
-                        Delay = 1
                     },
                     new ExternalStory.ExternalSay()
                     {
                         Who = "hacker",
                         What = "Don't touch my console.",
                         LoopTag = "squint",
-                        Delay = 1
                     },
                     new ExternalStory.ExternalSay()
                     {
                         Who = "goat",
                         What = "We do!",
                         LoopTag = "squint",
-                        Delay = 1
                     },
                     new ExternalStory.ExternalSay()
                     {
                         Who = "dizzy",
                         What = "Nah!",
-                        Delay = 1
                     },
                     new ExternalStory.ExternalSay()
                     {
                         Who = "riggs",
                         What = "Ah... yeah I do.",
                         LoopTag = "squint",
-                        Delay = 1
                     },
                 }
             ));
