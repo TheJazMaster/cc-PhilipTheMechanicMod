0c596a0309d4b53e6935a53b33000d37a036af13
clay <claytonharting+github@gmail.com>
2023-12-22 02:54:54 -0500

    fixed Precision Machining's tooltip

--- cards/PrecisionMechining.cs
+++ cards/PrecisionMechining.cs
@@ -18,13 +18,16 @@ namespace PhilipTheMechanic.cards
 
         public override List<CardAction> GetActions(State s, Combat c)
         {
+            int uncenterdAmount = upgrade == Upgrade.A ? 2 : 1;
+            int centeredAmount = upgrade == Upgrade.B ? 7 : 5;
+            int redrawAmount = uncenterdAmount;
+
             int index = c.hand.IndexOf(this);
-            int redrawAmount = upgrade == Upgrade.A ? 2 : 1;
             bool centered = c.hand.Count % 2 == 1 && index == c.hand.Count / 2;
 
             if (centered)
             {
-                redrawAmount = upgrade == Upgrade.B ? 7 : 5;
+                redrawAmount = centeredAmount;
             }
 
             List<CardAction> actions = new()
@@ -40,7 +43,7 @@ namespace PhilipTheMechanic.cards
                     tooltips = new() {
                         new TTText()
                         {
-                            text = $"If this card is in the center of your hand, add {redrawAmount} redraw and draw 2 cards, otherwise, add 1 redraw."
+                            text = $"If this card is in the center of your hand, add {centeredAmount} redraw and draw 2 cards, otherwise, add {uncenterdAmount} redraw."
                         },
                         new TTGlossary(MainManifest.glossary["CCardCentered"].Head),
                         new TTGlossary(MainManifest.glossary["CCardNotCentered"].Head),
@@ -48,7 +51,7 @@ namespace PhilipTheMechanic.cards
                     icons = new()
                     {
                         new Icon((Spr)MainManifest.sprites["icon_card_is_centered" + (centered? "" : "_disabled")].Id, null, Colors.textMain),
-                        new Icon((Spr)MainManifest.sprites["icon_redraw"].Id, upgrade == Upgrade.B ? 7 : 5, Colors.textMain),
+                        new Icon((Spr)MainManifest.sprites["icon_redraw"].Id, centeredAmount, Colors.textMain),
                         new Icon(Enum.Parse<Spr>("icons_drawCard"), 2, Colors.textMain)
                     }
                 },
@@ -57,7 +60,7 @@ namespace PhilipTheMechanic.cards
                     icons = new()
                     {
                         new Icon((Spr)MainManifest.sprites["icon_card_is_not_centered" + (centered? "_disabled" : "")].Id, null, Colors.textMain),
-                        new Icon((Spr)MainManifest.sprites["icon_redraw"].Id, upgrade == Upgrade.A ? 2 : 1, Colors.textMain)
+                        new Icon((Spr)MainManifest.sprites["icon_redraw"].Id, uncenterdAmount, Colors.textMain)
                     }
                 },
                 new ADummyAction(),
