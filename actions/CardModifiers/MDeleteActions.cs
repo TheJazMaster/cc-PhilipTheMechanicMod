927ced075d0374c31e9dab9e56e5e45a806628db
clay <claytonharting+github@gmail.com>
2024-02-08 18:23:41 -0500

    fixed MDeleteActions having a 0 next to it

--- actions/CardModifiers/MDeleteActions.cs
+++ actions/CardModifiers/MDeleteActions.cs
@@ -8,14 +8,13 @@ namespace clay.PhilipTheMechanic.Actions.CardModifiers
 {
     public class MDeleteActions : ICardModifier
     {
-        public int amount;
         public Spr? GetSticker(State s)
         {
             return ModEntry.Instance.sprites["icon_sticker_no_action"].Sprite;
         }
         public Icon? GetIcon(State s)
         {
-            return new Icon(ModEntry.Instance.sprites["icon_no_action"].Sprite, amount, Colors.textMain);
+            return new Icon(ModEntry.Instance.sprites["icon_no_action"].Sprite, null, Colors.textMain);
         }
         public List<CardAction> TransformActions(List<CardAction> actions, State s, Combat c)
         {
